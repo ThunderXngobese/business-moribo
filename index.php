f901995e271f384293347c931a68deaa00fb2b5d
ThunderXngobese <shounv998@gmail,com>
2021-02-05 00:02:03 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,312 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap" rel="stylesheet">
+
+    <title></title>
+
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+
+    <link rel="stylesheet" type="text/css" href="assets/css/font-awesome.css">
+
+    <link rel="stylesheet" href="assets/css/style.css">
+      <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    </head>
+    
+    <body>
+    
+ 
+    
+    
+    
+    <header class="header-area header-sticky">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <nav class="main-nav">
+                        
+                        <a href="index.html" class="logo">Moribo</a>
+                        
+                        <ul class="nav">
+                            <li><a href="#" class="active">Home</a></li>
+                            <li><a href="#">About</a></li>
+                            <li><a href="#">Services</a></li>
+                           
+                            <li><a href="#">Contact</a></li> 
+                        </ul>        
+                        <a class='menu-trigger'>
+                            <span>Menu</span>
+                        </a>
+                        
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+  
+
+    
+    <div class="main-banner" id="top">
+       
+            <img src="assets/images/fashion-designer.jpg" class="img-fluid yeahh" width="1350"/>
+        
+
+        <div class="video-overlay header-text">
+            <div class="caption">
+                <h6>Get the best fashion here</h6>
+                
+               
+            </div>
+        </div>
+    </div>
+    
+
+        <div class="contaner">
+            <br><br>
+        <center><h3>About Us</h3></center>
+            <br>
+            <p class="container">Moribo Creations Co-operative Limited it’s a company that endorse the value interest of fashion
+and design between the community through its integrated programmes, projects and products,
+design is currently taken as a career that requires high understanding of sewing machinery and is
+meant for the ones on skirts, our main focus is to change that discernment.
+The fact is that Fashion and Design are in the present age, omnipresent. Today possibly as a
+result of the eye-opening influence of trending clothing brands , people have started to that there
+is in fact Fashion and Design. Our brand expression encapsulates our stand on innovation tools
+regarding fashion and design for a better tomorrow, which is Carrying Your Own Cross.
+</p>
+        
+        
+        </div>
+        <br>
+        <div class="container">
+            <div class="row">
+        <div class="col-lg-4" id="Moses"><img src="assets/images/_108910399_carlings.line..jpg" class="img-fluid"></div>
+        <div class="col-lg-4" id="Moses"><img src="assets/images/clothes-rail.jpg" class="img-fluid"></div>
+        <div class="col-lg-4" id="Moses"><img src="assets/images/0d89ec94269e1c43.jpeg" class="img-fluid"></div>
+        
+        </div>
+        </div>
+        
+        <br>
+        <br>
+        <br><br>
+
+<div class="yebo">
+        
+    <div class="container"><br>
+    <h3><center>Our Services</center></h3>
+        <br>
+        <ul>
+        <li><b>Publications and manufacturing</b></li>
+            <li><b>Designing</b></li>
+            <li>Branded t-shirts</li>
+            <li>Toilored shirts and jackets</li>
+            <li>Designing and tailored trousers</li>
+            <li>Uniforms</li>
+            <li>Designed and tailored dresses</li>
+            <li>Corporate suits</li>
+            <li>Blouses</li>
+            <li>Traditional andwedding dresses</li>
+        
+        </ul>
+    
+    
+    </div>
+        
+            
+        </div><br><br>
+        
+        
+        <div class="container">
+        
+        <div class="row">
+            
+            <div class="col-6"><br><br><br><h3>Mission Statement</h3><p>Moribo Creations Co Operative Limited mission is to work to develop the quality of life through
+fashion and design innovations and provide opportunities for all to share in the world of
+tomorrow. Moribo Creations Co-Operative Limited is all about tailoring to individual style at
+affordable market related pricing. Producing quality yet stylish fabrics, tailored to the customer’s
+individual needs.
+</p><br><h3>Vision Statement</h3><p>Moribo Creation Co-Operative Limited vision is to be a standard establishment in harnessing
+innovation for trend, design, social and youth expansion.
+</p></div>
+            <div class="col-6"><img src="assets/images/1580832154_shutterstock_1040081671.jpg"></div>
+            
+            
+            
+            
+            
+            </div>
+        
+        
+        
+        
+        
+        
+        </div>
+        
+        
+        
+                <style>
+        
+        .tab-panel{
+    display: none;
+}
+
+.tab-panel .active{
+    display:block;
+}
+     #Moses{
+         margin: 0;
+         margin-top: 15px;
+                    }
+                    
+                    
+                    #code{
+                        margin:0;
+                        margin-right:0.5em;
+                    }
+                    
+                    
+                    .yebo{
+                        height: 55vh;
+                        background: fixed;
+                        background-image: url(assets/images/7-loza-maleombho-fashion-african-designers-timbuktu-travel.jpg);
+                       background-attachment: fixed;
+	background-position: top center;
+	background-repeat: no-repeat;
+	background-size: cover;
+	background-color: rgba(35,45,57,0.8);
+	cursor: default;
+	
+	width: 100%;
+	float: left;
+                        
+                        
+                    }
+        
+        
+        </style>
+        
+        <script>
+            
+            $('.tab-list').each(function(){
+    var $this = $(this);
+    var $tab = $this.find('li.active');
+    var $link = $tab.find('a');
+    var $panel = $($link.attr('href'));
+    
+    
+    $this.on('click','.tab-control', function(e){
+        e.preventDefault();
+        var $link = $(this);
+        var id = this.hash;
+        
+        
+        if(id && !$link.is('.active')){
+            $panel.removeClass('active');
+            $tab.removeClass('active');
+            
+            $panel = $(id).addClass('active');
+            $tab = $link.parent().addClass('active');
+        }
+        
+        
+    });
+    
+    
+    
+});
+            
+            
+            
+            
+        </script>
+        
+        <br><br><br>
+        <div class="container">
+        <center><h3>Contact us directly here</center></h3><br>
+          
+        </div>
+        <br>
+        <div class="container">
+        
+      Phone Numbers : +27 56 553 2323<br><br>
+            Email: <a href="mailto:email@gmail.com">Email@gmail.com</a><br><br>
+            Address:<br>
+            2321 John street<br>
+            Galeshewe<br>
+            8345<br>
+            Kimberley
+        
+        
+        
+        
+        </div>
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+   
+    <!-- ***** Footer Start ***** -->
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    
+                    <p class="float-right"><img src="assets/images/icon-facebook.svg" id="code"><img src="assets/images/icon-instagram.svg" id="code"><img src="assets/images/icon-twitter.svg" id="code"><img src="assets/images/icon-youtube.svg" id="code"></p>
+                 
+                    
+                    
+                    <p>
+                        Copyright © 2021 Moribo
+                        Designed by: <a href="#">SyntaxEra</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- jQuery -->
+    <script src="assets/js/jquery-2.1.0.min.js"></script>
+        
+        <script src="jquery/jquery.min.js "></script>
+<script src="../Logic/script.js"></script>
+    <script src="bootstrap/js/bootstrap.bundle.min.js "></script>
+    <script src="jquery.easing/jquery.easing.min.js "></script>
+
+        
+        <script src="script.js"></script>
+
+    <!-- Bootstrap -->
+    <script src="assets/js/popper.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+
+    <!-- Plugins -->
+    <script src="assets/js/scrollreveal.min.js"></script>
+    <script src="assets/js/waypoints.min.js"></script>
+    <script src="assets/js/jquery.counterup.min.js"></script>
+    <script src="assets/js/imgfix.min.js"></script> 
+    <script src="assets/js/mixitup.js"></script> 
+    <script src="assets/js/accordions.js"></script>
+    
+    <!-- Global Init -->
+    <script src="assets/js/custom.js"></script>
+
+  </body>
+</html>
